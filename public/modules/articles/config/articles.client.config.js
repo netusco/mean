e118763cbfd3b6b7c04872e933f839194ece18ff
netusco <ernestconill@gmail.com>
2014-05-10 13:25:20 +0200

    Calling menus or submenus using same addMenuItem()

--- public/modules/articles/config/articles.client.config.js
+++ public/modules/articles/config/articles.client.config.js
@@ -5,7 +5,9 @@ angular.module('articles').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Articles', 'articles', 'dropdown');
-		Menus.addSubMenuItem('topbar', 'articles', 'List Articles', 'articles');
-		Menus.addSubMenuItem('topbar', 'articles', 'New Article', 'articles/create');
+        	Menus.addMenuItem('topbar', 'All Articles', 'articles', 'submenu', 1, 'articles');
+	        Menus.addMenuItem('topbar', 'Editor', 'articles/editor', 'submenu', 1, 'articles');
+	        Menus.addMenuItem('topbar', 'Add Article', 'articles/create', 'submenu', 2, 'articles/editor');
+	        Menus.addMenuItem('topbar', 'New Article', 'articles/create', 'submenu', 3, 'articles/create');
 	}
 ]);
