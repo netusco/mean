213c326e477a09e3697324283d2fd474052e5634
netusco <ernestconill@gmail.com>
2014-05-10 13:28:36 +0200

    merging addMenuItem with addSubMenuItem

--- public/modules/core/services/menus.client.service.js
+++ public/modules/core/services/menus.client.service.js
@@ -73,51 +73,68 @@ angular.module('core').service('Menus', [
 			delete this.menus[menuId];
 		};
 
-		// Add menu item object
-		this.addMenuItem = function(menuId, menuItemTitle, menuItemURL, menuClass, menuItemUIRoute, isPublic, roles) {
-			// Validate that the menu exists
-			this.validateMenuExistance(menuId);
-
-			// Push new menu item
-			this.menus[menuId].items.push({
-				class: (menuClass === 'dropdown') ? menuClass : '',
-				title: menuItemTitle,
-				link: menuItemURL,
-				uiRoute: menuItemUIRoute || ('/' + menuItemURL),
-				isPublic: isPublic || this.menus[menuId].isPublic,
-				roles: roles || this.defaultRoles,
-                		subitems: [],
-				shouldRender: shouldRender
-			});
-
-			// Return the menu object
-			return this.menus[menuId];
-		};
-		
-		// Add submenu item object
-	        this.addSubMenuItem = function(menuId, rootMenuItemURL, menuItemTitle, menuItemURL, menuItemUIRoute, isPublic, roles) {
+	        // Add menu item object
+	        this.addMenuItem = function(menuId, menuItemTitle, menuItemURL, menuClass, level, parentMenuItemURL, menuItemUIRoute, isPublic, roles) {
 	            // Validate that the menu exists
 	            this.validateMenuExistance(menuId);
-	
-	            // Search for menu item
-	            for (var itemIndex in this.menus[menuId].items) {
-	                if (this.menus[menuId].items[itemIndex].link === rootMenuItemURL) {
-	                    // Push new submenu item
-	                    this.menus[menuId].items[itemIndex].subitems.push({
-	                        title: menuItemTitle,
-	                        link: menuItemURL,
-	                        uiRoute: menuItemUIRoute || ('/' + menuItemURL),
-	                        isPublic: isPublic || this.menus[menuId].isPublic,
-	                        roles: roles || this.defaultRoles,
-	                        shouldRender: shouldRender
-	                    });
+	            
+	            var menu = {
+	                    title: menuItemTitle,
+	                    link: menuItemURL,
+	                    class: menuClass || '',
+	                    level: level || 1,
+	                    uiRoute: menuItemUIRoute || ('/' + menuItemURL),
+	                    isPublic: isPublic || this.menus[menuId].isPublic,
+	                    roles: roles || this.defaultRoles,
+	                    subitems: [],
+	                    shouldRender: shouldRender
+	                },
+	                menuItems = this.menus[menuId].items;
+	            
+	            if (menuClass === 'submenu') {
+	                // Search for menu item
+	                for (var itemIndex in menuItems) {
+	                    switch (level) {
+	                        case (1):
+	                            if (menuItems[itemIndex].link === parentMenuItemURL) {
+	                                // Push new menu item
+	                                menuItems[itemIndex].subitems.push(menu);
+	                            }
+	                            break;
+	                        case (2):
+	                            for (var subitemIndex in menuItems[itemIndex].subitems) {
+	                                if (menuItems[itemIndex].subitems[subitemIndex].link === parentMenuItemURL) {
+	                                    // Change parent class to dropdown-submenu
+	                                    menuItems[itemIndex].subitems[subitemIndex].class = 'dropdown-submenu';
+	                                    
+	                                    // Push new menu item
+	                                    menuItems[itemIndex].subitems[subitemIndex].subitems.push(menu);
+	                                }
+	                            }
+	                            break;
+	                        case (3):
+	                            for (var resubitemIndex in menuItems[itemIndex].subitems) {
+	                                for (var subSubitemIndex in menuItems[itemIndex].subitems[resubitemIndex].subitems) {
+	                                    if (menuItems[itemIndex].subitems[resubitemIndex].subitems[subSubitemIndex].link === parentMenuItemURL) {
+	                                        // Change parent class to dropdown-submenu
+	                                        menuItems[itemIndex].subitems[resubitemIndex].subitems[subSubitemIndex].class = 'dropdown-submenu';
+	                                    
+	                                        // Push new menu item
+	                                        menuItems[itemIndex].subitems[resubitemIndex].subitems[subSubitemIndex].subitems.push(menu);
+	                                    }
+	                                }
+	                                
+	                            }
+	                            break; 
+	                    }
 	                }
-	            }                        
-	
+	            } else {
+	                menuItems.push(menu);
+	            }
 	            // Return the menu object
 	            return this.menus[menuId];
 	        };
-
+	        
 		// Remove existing menu object by menu id
 		this.removeMenuItem = function(menuId, menuItemURL) {
 			// Validate that the menu exists
@@ -142,7 +159,7 @@ angular.module('core').service('Menus', [
 	            // Search for menu item to remove
 	            for (var itemIndex in this.menus[menuId].items) {
 	                for (var subitemIndex in this.menus[menuId].items[itemIndex].subitems) {
-	                    if (this.menus[menuId].items[itemIndex].subitems[subitemIndex].link === menuItemURL) {
+	                    if (this.menus[menuId].items[itemIndex].subitems[subitemIndex].link === submenuItemURL) {
 	                        this.menus[menuId].items[itemIndex].subitems.splice(subitemIndex, 1);
 	                    }
 	                }
